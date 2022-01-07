fa7fd04e7afd0564793018e65896ebefde4d7b1a
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-01-07 19:37:23 +0000

    more refactoring

--- src/shop.js
+++ src/shop.js
@@ -4,77 +4,73 @@ class Shop {
     this.items = items;
   }
 
-  newDay() {
-    this.updateSellIn();
-    this.updateQuality();
+  // newDay() {
+  //   this.updateSellIn();
+  //   this.updateQuality();
 
-    return this.items;
+  //   return this.items;
+  // }
+
+  // updateSellIn() {
+  //   this.items.forEach((item) => {
+  //     item.name != "Sulfuras, Hand of Ragnaros" ? item.sellIn-- : null;
+  //   });
+  // }
+
+  updateBrie(item) {
+    if (item.quality < 50) {
+      item.quality++;
+    }
+    if (item.quality < 50 && item.sellIn < 0) {
+      item.quality++;
+    }
   }
 
-  updateSellIn() {
-    this.items.forEach((item) => {
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn -= 1;
-      }
-    });
-  }
-
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
+  updateBackstagePasses(item) {
+    if (item.quality < 50) {
+      if (item.sellIn < 0) {
+        item.quality = 0;
+      } else if (item.sellIn < 6) {
+        item.quality += 3;
+      } else if (item.sellIn < 11) {
+        item.quality += 2;
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      // if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-      //   this.items[i].sellIn = this.items[i].sellIn - 1;
-      // }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+        item.quality++;
       }
     }
+  }
 
-    // return this.items;
+  newDay() {
+    this.items.forEach((item) => {
+      if (item.name == "Sulfuras, Hand of Ragnaros") {
+        return;
+      } else {
+        item.sellIn--;
+
+        // if (item.name == "Aged Brie") {
+        //   this.updateBrie(item);
+        // } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        //   this.updateBackstagePasses(item);
+        // } else {
+        //   item.quality > 0 ? item.quality-- : null;
+
+        //   item.sellIn < 0 && item.quality > 0 ? item.quality-- : null;
+        // }
+        switch (item.name) {
+          case "Aged Brie":
+            this.updateBrie(item);
+            break;
+          case "Backstage passes to a TAFKAL80ETC concert":
+            this.updateBackstagePasses(item);
+            break;
+          default:
+            item.quality > 0 ? item.quality-- : null;
+            item.sellIn < 0 && item.quality > 0 ? item.quality-- : null;
+        }
+      }
+    });
+
+    return this.items;
   }
 }
 
